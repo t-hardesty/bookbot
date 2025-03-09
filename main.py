d44f1c5196cfdc6c45f23ef64d146c628e5bbd74
t-hardesty <taylorhardesty22.com>
2025-03-09 08:52:22 -0600

    corrected file title

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_count = 0
+        words = file_contents.split()
+        for word in words:
+            word_count += 1
+    return print(word_count)
+main()
